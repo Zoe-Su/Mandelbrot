7e87ba6b4a6be78f0229d235e6a0192ea98a88d7
Zoe-Su <150188389+Zoe-Su@users.noreply.github.com>
2023-11-18 04:29:16 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// C++ Libraries
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+  return 0;
+}
